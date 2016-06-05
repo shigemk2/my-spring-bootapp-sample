fa8ddddc8bece1da83ba37e4b54c08012a935938
shigemk2 <i.am.shige@gmail.com>
2016-06-06 03:25:01 +0900

    Use ModelAndView

--- src/main/java/com/shigemk2/libro/bootapp/SampleController.java
+++ src/main/java/com/shigemk2/libro/bootapp/SampleController.java
@@ -2,8 +2,8 @@ package com.shigemk2.libro.bootapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SampleController {
@@ -12,8 +12,9 @@ public class SampleController {
     MyDataRepository repository;
 
     @RequestMapping("/hello")
-    public String hello(Model model) {
-        model.addAttribute("msg", "これはThymeleafを使ったサンプルです。");
-        return "hello";
+    public ModelAndView hello(ModelAndView mav) {
+        mav.setViewName("hello");
+        mav.addObject("msg", "これはThymeleafを使ったサンプルです。");
+        return mav;
     }
 }
