21adb7bee08ca4b94431a5d13661e3d2bc324bf6
shigemk2 <i.am.shige@gmail.com>
2016-06-06 03:22:20 +0900

    Add Thymeleaf template engine

--- src/main/java/com/shigemk2/libro/bootapp/SampleController.java
+++ src/main/java/com/shigemk2/libro/bootapp/SampleController.java
@@ -1,14 +1,9 @@
 package com.shigemk2.libro.bootapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Iterator;
 
 @Controller
 public class SampleController {
@@ -18,29 +13,7 @@ public class SampleController {
 
     @RequestMapping("/hello")
     public String hello(Model model) {
-        Iterable<MyData> list = repository.findAll();
-        model.addAttribute("datas", list);
-        return "hello";
-    }
-
-    @RequestMapping(value = "/post", method = RequestMethod.POST)
-    public String hello(Model model,
-                        @RequestParam("name") String name,
-                        @RequestParam("mail") String mail,
-                        @RequestParam("tel") String tel,
-                        @RequestParam("age") int age) {
-        MyData mydata = new MyData(name, mail, tel, age);
-        repository.saveAndFlush(mydata);
-        Iterable<MyData> list = repository.findAll();
-        model.addAttribute("datas", list);
-        return "hello";
-    }
-
-    @RequestMapping(value = "/find", method = RequestMethod.POST)
-    public String find(Model model,
-                       @RequestParam("find") String fstr) {
-        Iterable<MyData> list = repository.findByName(fstr);
-        model.addAttribute("datas", list);
+        model.addAttribute("msg", "これはThymeleafを使ったサンプルです。");
         return "hello";
     }
 }
