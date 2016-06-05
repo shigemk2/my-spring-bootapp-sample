c115e98f92d12631c45046824e5ec7fdbb354fc9
shigemk2 <i.am.shige@gmail.com>
2016-06-05 14:20:15 +0900

    Add hello page

--- src/main/java/com/shigemk2/libro/bootapp/SampleController.java
+++ src/main/java/com/shigemk2/libro/bootapp/SampleController.java
@@ -1,13 +1,13 @@
 package com.shigemk2.libro.bootapp;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SampleController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Spring Boot Sample!";
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,14 @@
+<%@page language="java" contentType="text/html; charset=utf-8"
+pageEncoding="utf-8" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Hello Page</title>
+</head>
+<body>
+<h1>Hello!</h1>
+<p>これはサンプルのページです。</p>
+</body>
+</html>
