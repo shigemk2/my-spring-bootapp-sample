db3c8d6eaaf76722a656b475a55a0f70d445a956
shigemk2 <i.am.shige@gmail.com>
2016-06-05 12:38:58 +0900

    Initial commit

---
+++ src/main/java/com/shigemk2/libro/bootapp/App.java
@@ -0,0 +1,13 @@
+package com.shigemk2.libro.bootapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
