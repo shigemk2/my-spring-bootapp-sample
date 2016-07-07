e9b657850eb02e0d1f20bcb928ff36b45512dfe0
shigemk2 <i.am.shige@gmail.com>
2016-07-07 23:34:14 +0900

    Initialize mongodb data

--- src/main/java/com/shigemk2/libro/bootapp/App.java
+++ src/main/java/com/shigemk2/libro/bootapp/App.java
@@ -1,5 +1,7 @@
 package com.shigemk2.libro.bootapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App implements CommandLineRunner {
+    @Autowired
+    private MyMongoRepository mongoRepository;
+
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        mongoRepository.deleteAll();
+        mongoRepository.save(new MongoData("hello", "this is sample memo."));
+        mongoRepository.save(new MongoData("check!", "check sample code..."));
+        mongoRepository.save(new MongoData("サンプルメモ", "これはサンプルのメモです"));
     }
 }
